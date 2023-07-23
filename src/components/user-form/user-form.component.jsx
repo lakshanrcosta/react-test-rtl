ecf56f4cf88b637d7c54b0e33080aa0114c3e80d
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-23 22:02:06 +0300

    Added state for user name and email and added event handlers for each actions

--- src/components/user-form/user-form.component.jsx
+++ src/components/user-form/user-form.component.jsx
@@ -1,19 +1,46 @@
+import { useState } from 'react';
 import './user-form.styles.css';
 
 const UserForm = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  const handleUserNameOnChange = (event) => setName(event.target.value);
+
+  const handleUserEmailOnChange = (event) => setEmail(event.target.value);
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, email);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <div className='container p-3'>
         <div className='form-group'>
           <label htmlFor='user-name'>User name</label>
-          <input id='user-name' type='text' className='form-control' placeholder='Enter email' />
+          <input
+            id='user-name'
+            type='text'
+            value={name}
+            className='form-control'
+            placeholder='Enter email'
+            onChange={handleUserNameOnChange}
+          />
         </div>
         <div className='form-group mb-2'>
           <label htmlFor='user-email'>User email</label>
-          <input id='user-email' type='email' className='form-control' placeholder='Password' />
+          <input
+            id='user-email'
+            type='email'
+            value={email}
+            className='form-control'
+            placeholder='Password'
+            onChange={handleUserEmailOnChange}
+          />
         </div>
         <button type='submit' className='btn btn-primary'>
-          Submit
+          Add user
         </button>
       </div>
     </form>
