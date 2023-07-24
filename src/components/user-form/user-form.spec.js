2e049d3a2f15528c351d0d98903b949a5f40baa2
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-24 17:43:46 +0300

    Added test for onUserAdd function

--- src/components/user-form/user-form.spec.js
+++ src/components/user-form/user-form.spec.js
@@ -15,4 +15,36 @@ describe('UserForm component test suite', () => {
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
   });
+
+  it('It should call onUserAdd when the form is submitted', () => {
+    // Callback function
+    const argList = [];
+    const callback = (...args) => {
+      argList.push(args);
+    };
+
+    // Render the component
+    render(<UserForm onUserAdd={callback} />);
+
+    // Find two text inputs
+    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+    // Simulate typing in a name
+    user.click(nameInput);
+    user.keyboard('John');
+
+    // Simulate typing in an email
+    user.click(emailInput);
+    user.keyboard('john.doe@unknown.com');
+
+    // Find the button
+    const button = screen.getByRole('button');
+
+    // Simulate the button click
+    user.click(button);
+
+    // Assertion
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({ name: 'John', email: 'john.doe@unknown.com' });
+  });
 });
